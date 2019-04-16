b73259011c24dab6e5d281d0baed3d9aff7338d1
ierc <eric.huecker@gmail.com>
2019-04-16 14:00:51 -0700

    Add files via upload

---
+++ pulse/worshipteam/worshipteam.php
@@ -0,0 +1,74 @@
+<?php include("/var/www/vhosts/fumcbedford.com/httpdocs/youth/password_protect.php"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Untitled Document</title>
+
+<style type="text/css">
+<!--
+body,td,th {
+	font-size: 12px;
+	font-family: Arial, Helvetica, sans-serif;
+}
+a:link {
+	color: #0099FF;
+}
+a:visited {
+	color: #0099FF;
+}
+a:hover {
+	color: #66FFFF;
+}
+a:active {
+	color: #66FFFF;
+}
+-->
+</style>
+
+
+</head>
+
+<body text="#DDDDDD">
+<table width="685" height="349" align="center">
+  <tr>  <td background="../images/wteambox.png" width="681" height="349" valign="top">
+<img src="../images/spacer.gif" height=65 /><br />
+
+<div style="width:650px;height:260px;overflow:auto;" align="center">
+
+<table align=center width="650">
+<tr>
+
+<td width=40></td>
+
+<td align="left" valign="top" width=325>
+
+<h3><u >03.16.09 Set </u><a href="rss.rss"></a></h3>
+<p>
+
+<a href="theanswer.pdf" target="_Blank">The Answer</a>  <p>
+
+<a href="fieldsofgrace.pdf" target="_Blank">Fields of Grace</a>  <p>
+
+<a href="fromtheinsideout.pdf" target="_Blank">From The Inside Out</a>  <p>
+
+<a href="#" target="_Blank">???</a>  <p>
+
+</td><td width=300 valign="top" align=right>
+<div align="right">
+
+<embed style="width:300px; visibility:visible; height:250px;" allowScriptAccess="never" src="http://www.profileplaylist.net/mc/mp3player_new.swf" flashvars="config=http%3A%2F%2Fwww.indimusic.us%2Fext%2Fpc%2Fconfig_black_noautostart.xml&amp;mywidth=300&amp;myheight=250&amp;playlist_url=http://www.indimusic.us/loadplaylist.php?playlist=60398056&t=1236712090&amp;wid=os" width="300" height="250" name="mp3player" wmode="transparent" type="application/x-shockwave-flash" border="0"/ align=right>
+
+</td>
+
+
+</div>
+
+
+
+</td></tr></table>
+
+
+
+</body>
+</html>
